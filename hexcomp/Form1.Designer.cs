041fda8e15f5401d83f5724f5a3473e42c2f8849
Sergey.Azarenko <azarenko.s@gmail.com>
2019-02-07 14:48:49 +0200

    the first release

---
+++ hexcomp/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace hexcomp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.FileList = new System.Windows.Forms.TextBox();
+            this.Add = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.Compare = new System.Windows.Forms.Button();
+            this.grid = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox2, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 144F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1035, 659);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Compare);
+            this.groupBox1.Controls.Add(this.Clear);
+            this.groupBox1.Controls.Add(this.Add);
+            this.groupBox1.Controls.Add(this.FileList);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1029, 138);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Files to compare";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.grid);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(3, 147);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1029, 509);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "File Content";
+            // 
+            // FileList
+            // 
+            this.FileList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FileList.Location = new System.Drawing.Point(9, 19);
+            this.FileList.Multiline = true;
+            this.FileList.Name = "FileList";
+            this.FileList.ReadOnly = true;
+            this.FileList.Size = new System.Drawing.Size(1011, 81);
+            this.FileList.TabIndex = 0;
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(9, 106);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(75, 23);
+            this.Add.TabIndex = 1;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(90, 106);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(75, 23);
+            this.Clear.TabIndex = 2;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Compare
+            // 
+            this.Compare.Location = new System.Drawing.Point(171, 106);
+            this.Compare.Name = "Compare";
+            this.Compare.Size = new System.Drawing.Size(75, 23);
+            this.Compare.TabIndex = 3;
+            this.Compare.Text = "Compare";
+            this.Compare.UseVisualStyleBackColor = true;
+            this.Compare.Click += new System.EventHandler(this.Compare_Click);
+            // 
+            // grid
+            // 
+            this.grid.AllowUserToAddRows = false;
+            this.grid.AllowUserToDeleteRows = false;
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grid.Location = new System.Drawing.Point(3, 16);
+            this.grid.Name = "grid";
+            this.grid.ReadOnly = true;
+            this.grid.Size = new System.Drawing.Size(1023, 490);
+            this.grid.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1035, 659);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "HexComp by Azarenko S.A.";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox FileList;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button Compare;
+        private System.Windows.Forms.DataGridView grid;
+    }
+}
+
---
+++ hexcomp/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace hexcomp
+{
+    public partial class Form1 : Form
+    {
+        DataTable table = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 16; i++)
+            {
+                DataColumn column = new DataColumn(i.ToString("X2"));
+                table.Columns.Add(column);
+            }
+
+            grid.DataSource = table;
+
+            foreach (DataGridViewColumn column in grid.Columns)
+            {
+                column.AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            }
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                FileList.Text += openFileDialog1.FileName + "\r\n";
+            }
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            FileList.Clear();
+        }
+
+        private void Compare_Click(object sender, EventArgs e)
+        {
+            string[] files = FileList.Text.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            List<FileStream> streams = new List<FileStream>(files.Length);
+
+            foreach(string filepath in files)
+            {
+                if (File.Exists(filepath))
+                {
+                    FileStream fs = new FileStream(filepath, FileMode.Open);
+                    streams.Add(fs);
+                }
+            }
+
+            table.Clear();
+
+            byte[] currentBytes = new byte[streams.Count];
+            string[] currentConvertedBytes = new string[streams.Count];
+            List<int> shouldMarked = new List<int>();            
+            
+            do
+            {
+                DataRow row = table.NewRow();
+                shouldMarked.Clear();
+
+                for (int i = 0; i < 16; i++)
+                {
+                    for (int streamIndex = 0; streamIndex < streams.Count; streamIndex++)
+                    {
+                        if (streams[streamIndex].Position == streams[streamIndex].Length)
+                        {
+                            goto endofcycle;
+                        }
+
+                        currentBytes[streamIndex] = (byte)streams[streamIndex].ReadByte();
+                        currentConvertedBytes[streamIndex] = currentBytes[streamIndex].ToString("X2");
+                    }
+
+                    for (int j = 1; j < currentBytes.Length; j++)
+                    {
+                        if (currentBytes[j - 1] != currentBytes[j])
+                        {
+                            shouldMarked.Add(i);
+                            break;
+                        }
+                    }
+
+                    row[i] = string.Join(" ", currentConvertedBytes);                                        
+                }                        
+
+                table.Rows.Add(row);
+                DataGridViewRow dgvr = grid.Rows[table.Rows.Count - 1];
+                dgvr.HeaderCell.Value = ((table.Rows.Count - 1) * 16).ToString("X6");
+
+                foreach (int cellIndex in shouldMarked)
+                {
+                    DataGridViewCell cell = dgvr.Cells[cellIndex];
+                    cell.Style.ForeColor = Color.Blue;
+                }
+
+                continue;
+
+                endofcycle:
+                break;
+            }
+            while(true);
+
+            foreach (FileStream fs in streams)
+            {
+                fs.Close();
+                fs.Dispose();
+            }
+        }
+    }
+}
